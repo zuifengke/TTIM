0b90e0a57d77c93a27437b3e51201c136a5638bd
zuifengke <yehui1989@qq.com>
2015-09-22 14:07:18 +0800

    提交消息处理类

---
+++ Client.Winform/Utility/MessageHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Client.Winform.Utility
+{
+    /// <summary>
+    /// 消息处理
+    /// </summary>
+    public class MessageHandler
+    {
+        private MessageHandler m_Instance = null;
+        public  MessageHandler Instance
+        {
+            get {
+                if (this.m_Instance == null)
+                    this.m_Instance = new MessageHandler();
+                return this.m_Instance;
+            }
+            set
+            {
+                this.m_Instance = value;
+            }
+        }
+
+    }
+}
